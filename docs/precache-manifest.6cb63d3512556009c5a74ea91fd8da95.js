6cdc9963b788a06d4ac0a693f333721da176ae6e
Benji-Mtz <benjim.flores@outlook.com>
2020-07-05 22:52:54 -0500

    docs agregado

---
+++ docs/precache-manifest.6cb63d3512556009c5a74ea91fd8da95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b7c93fd210513ec1ba0f9d6ca22dafb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54927d952c89a3344b56",
+    "url": "/static/css/main.053e1c64.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54927d952c89a3344b56",
+    "url": "/static/js/main.736ec4e7.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
